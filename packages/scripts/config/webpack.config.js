7bd9bcdf646d124479428f87cd07314207cb0f78
Andrés <andres.maneiro@automattic.com>
2019-02-14 16:20:44 +0100

    Teach webpack where to lookup loaders
    It'll look them up in the scripts package's node_modules,
    not in the consumer's node_modules.

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -2,6 +2,7 @@
 * External dependencies
 */
 const path = require( 'path' );
+const PnpWebpackPlugin = require( 'pnp-webpack-plugin' );
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -12,16 +13,26 @@ module.exports = {
 		path: path.resolve( '.' ),
 		filename: 'index.build.js',
 	},
+	resolve: {
+		plugins: [
+			PnpWebpackPlugin,
+		],
+	},
+	resolveLoader: {
+		plugins: [
+			PnpWebpackPlugin.moduleLoader( module ),
+		],
+	},
 	module: {
 		rules: [
 			{
 				test: /.js$/,
 				exclude: /node_modules/,
 				use: [ {
-					loader: 'babel-loader',
+					loader: require.resolve( 'babel-loader' ),
 					options: {
 						plugins: [
-							[ '@babel/plugin-transform-react-jsx', {
+							[ require.resolve( '@babel/plugin-transform-react-jsx' ), {
 								pragma: 'wp.element.createElement',
 							} ],
 						],
